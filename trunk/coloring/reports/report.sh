c730dc9f4794132a288c857b1ba6bc1cc2318c3b
seuvitor <seuvitor@a840bba9-3251-0410-9ac1-13671e7434a1>
2008-07-08 19:33:44 +0000

    Fixed script to include time in output file name.

--- trunk/coloring/reports/report.sh
+++ trunk/coloring/reports/report.sh
@@ -1,7 +1,8 @@
 # This script runs the specified coloring algorithm over the complete
 # set of instances, generating an output file with the results.
 if [ $# -ne 1 ]; then echo 1>&2 Usage: $0 ALGORITHM_NAME; exit; fi
+TIMESTAMP=`date +%Y-%m-%d_%Hh%M`
 cd ../instances/
 for file in *.col
 do ../debug/coloring ${1} ${file}
-done > ../reports/${1}_output.txt
+done > ../reports/${1}_output_${TIMESTAMP}.txt
