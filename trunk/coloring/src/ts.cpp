2fcbda4d17fa9a54292dd703a8875c37545ef615
seuvitor <seuvitor@a840bba9-3251-0410-9ac1-13671e7434a1>
2008-08-24 16:18:19 +0000

    Refactoring: now using references instead of pointers whenever possible.

--- trunk/coloring/src/ts.cpp
+++ trunk/coloring/src/ts.cpp
@@ -7,15 +7,15 @@
 
 #define K 16
 
-void generateRandomSolution(Instance* instance, Solution* solution, char** countAdjColors, std::set<int>& conflictingVertices)
+void generateRandomSolution(Solution& solution, char** countAdjColors, std::set<int>& conflictingVertices)
 {
-    for (int u = 0; u < instance->nvertices; ++u)
+    for (int u = 0; u < solution.instance->nvertices; ++u)
     {
         int color = rand() % K;
-        solution->coloring[u] = color;
+        solution.coloring[u] = color;
 
         // Gets adjacency of vertex u
-        int* adj = instance->gamma[u];
+        int* adj = solution.instance->gamma[u];
         
         // Iteration starts on index 1 and ends adj[0] indices after the start
         for (int *it = (adj + 1), *end = (it + adj[0]); it != end; ++it)
@@ -25,22 +25,22 @@ void generateRandomSolution(Instance* instance, Solution* solution, char** count
         }
     }
     
-    for (int u = 0; u < instance->nvertices; ++u)
+    for (int u = 0; u < solution.instance->nvertices; ++u)
     {
-        if (countAdjColors[u][solution->coloring[u]] != 0)
+        if (countAdjColors[u][solution.coloring[u]] != 0)
         {
             conflictingVertices.insert(u);
         }
     }
 }
 
-int calculateValue(Instance* instance, Solution* solution)
+int calculateValue(const Solution& solution)
 {
     int value = 0;
-    for (int u = 0; u < instance->nvertices; ++u)
+    for (int u = 0; u < solution.instance->nvertices; ++u)
     {
         // Gets adjacency of vertex u
-        int* adj = instance->gamma[u];
+        int* adj = solution.instance->gamma[u];
         
         // Iteration starts on index 1 and ends adj[0] indices after the start
         for (int *it = (adj + 1), *end = (it + adj[0]); it != end; ++it)
@@ -48,7 +48,7 @@ int calculateValue(Instance* instance, Solution* solution)
             int v = *it;
             if (u < v)
             {
-                if (solution->coloring[u] == solution->coloring[v])
+                if (solution.coloring[u] == solution.coloring[v])
                     value += 1;
             }            
         }
@@ -56,16 +56,16 @@ int calculateValue(Instance* instance, Solution* solution)
     return value;
 }
 
-void chooseBestMove(const Solution* solution, char** countAdjColors,
+void chooseBestMove(const Solution& solution, char** countAdjColors,
         const std::set<int>& conflictingVertices, const int diffToBestValue,
-        std::list<std::pair<int, int> >& tabuList,
+        const std::list<std::pair<int, int> >& tabuList,
         std::pair<int, int>& bestMove, int& bestMoveDelta)
 {
     std::pair<int, int> move;
     for (std::set<int>::iterator vertexIt = conflictingVertices.begin(); vertexIt != conflictingVertices.end(); ++vertexIt)
     {
         int u = *vertexIt;
-        int currentColor = solution->coloring[u];
+        int currentColor = solution.coloring[u];
         int currentConflicts = countAdjColors[u][currentColor];
         for (int newColor = 0; newColor < K; ++newColor)
         {
@@ -102,20 +102,20 @@ void chooseBestMove(const Solution* solution, char** countAdjColors,
 
 }
 
-void applyMove(Solution* solution, char** countAdjColors,
-        std::set<int>& conflictingVertices, std::pair<int, int>& move)
+void applyMove(Solution& solution, char** countAdjColors,
+        std::set<int>& conflictingVertices, const std::pair<int, int>& move)
 {
     int u = move.first;
     int newColor = move.second;
-    int oldColor = solution->coloring[u];
+    int oldColor = solution.coloring[u];
     
     // Change color of vertex
-    solution->coloring[u] = newColor;
+    solution.coloring[u] = newColor;
     
     // Update count of adjacent colors
     
     // Gets adjacency of vertex u
-    int* adj = solution->instance->gamma[u];
+    int* adj = solution.instance->gamma[u];
     
     // Iteration starts on index 1 and ends adj[0] indices after the start
     for (int *it = (adj + 1), *end = (it + adj[0]); it != end; ++it)
@@ -125,13 +125,13 @@ void applyMove(Solution* solution, char** countAdjColors,
         countAdjColors[v][newColor] += 1;
         
         // If this move removes the last conflict of vertex v
-        if (solution->coloring[v] == oldColor
+        if (solution.coloring[v] == oldColor
                 && countAdjColors[v][oldColor] == 0)
         {
             conflictingVertices.erase(v);
         }
         // If this move creates the first conflict for vertex v
-        else if (solution->coloring[v] == newColor
+        else if (solution.coloring[v] == newColor
                 && countAdjColors[v][newColor] == 1)
         {
             conflictingVertices.insert(v);
@@ -156,8 +156,8 @@ void ts_constructSolution(Instance* instance, Solution* solution)
     }
     
     Solution currentSolution(instance);
-    generateRandomSolution(instance, &currentSolution, countAdjColors, conflictingVertices);
-    int currentValue = calculateValue(instance, &currentSolution);
+    generateRandomSolution(currentSolution, countAdjColors, conflictingVertices);
+    int currentValue = calculateValue(currentSolution);
     
     Solution bestSolution = currentSolution;
     int bestValue = currentValue;
@@ -173,14 +173,14 @@ void ts_constructSolution(Instance* instance, Solution* solution)
     {
         std::pair<int, int> bestMove = std::make_pair(-1, -1);
         int bestMoveDelta = -1;
-        chooseBestMove(&currentSolution, countAdjColors, conflictingVertices,
+        chooseBestMove(currentSolution, countAdjColors, conflictingVertices,
                 diffToBestValue, tabuList, bestMove, bestMoveDelta);
         
         // Append to tabu list and move, if any non-tabu movement was available
         if (bestMove.first != -1)
         {
             tabuList.push_back(bestMove);
-            applyMove(&currentSolution, countAdjColors, conflictingVertices, bestMove);
+            applyMove(currentSolution, countAdjColors, conflictingVertices, bestMove);
             currentValue += bestMoveDelta;
         }
         
