551187f715c6d9ba1ebcd9776fdeec639002ce10
seuvitor <seuvitor@users.noreply.github.com>
2008-08-25 15:51:00 +0000

    Introduced script to compile source and run a coloring algorithm over the complete set of instances, printing output to the screen.

---
+++ coloring/reports/run.sh
@@ -0,0 +1,10 @@
+# This script runs the specified coloring algorithm over the complete
+# set of instances, generating an output file with the results.
+if [ $# -ne 1 ]; then echo 1>&2 Usage: $0 ALGORITHM_NAME; exit; fi
+TIMESTAMP=`date +%Y-%m-%d_%Hh%M`
+cd ../debug/
+make
+cd ../instances/
+for file in *.col
+do ../debug/coloring ${1} ${file}
+done
